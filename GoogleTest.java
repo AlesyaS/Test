9cf208fda8a93411d73588ca47db3a13ea5de224
AlesyaS <alsarakvash@gmail.com>
2016-07-18 12:57:06 +0300

    Update GoogleTest.java

--- GoogleTest.java
+++ GoogleTest.java
@@ -1,9 +1,8 @@
 import org.openqa.selenium.By;
-//import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class GoogleTest {
 public static void main(String[] args) throws InterruptedException{
